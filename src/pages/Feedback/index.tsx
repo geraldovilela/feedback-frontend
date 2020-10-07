644ee8ef08788f16468fb976ca5a216c67011cf5
geraldo vilela <geraldovilela19@gmail.com>
2020-10-06 23:46:47 -0400

    dev: setting request for data.

--- src/pages/Feedback/index.tsx
+++ src/pages/Feedback/index.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React,{useEffect, useState, useCallback} from 'react';
+
 import Feedbacks from '../../components/Feedbacks/Feedbacks';
 import PageHeader from '../../components/PageHeader';
-
+import api from '../../services/api';
 
 export default function Feedback() {
+    const [feedbacks, setFeedbacks]= useState({})
+    const id = localStorage.getItem('login')
+    const getFeedbacks= useCallback( async ()=>{
+        const data = await api.get('/feedback').then()
+        setFeedbacks(data);
+        
+    },[]);
+    
+    useEffect(()=>{
+        getFeedbacks()
+    }, [getFeedbacks])
+
+   
+
+
     return (
         <>
             <PageHeader />
             <div id="container">
                 <main id="container">
+                 
                     <Feedbacks></Feedbacks>
-                    <Feedbacks></Feedbacks>
-                    <Feedbacks></Feedbacks>
-                    <Feedbacks></Feedbacks>
+                
                 </main>
             </div>
         </>
